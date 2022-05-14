48515856f0cf2f010bdf89087b7cbac3d8ff3670
HJfod <60038575+HJfod@users.noreply.github.com>
2022-05-14 12:48:38 +0300

    move main.cpp to src + other small fixes and comments

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <Geode.hpp>
-
-class $modify(MenuLayer) {
-	void onMoreGames(cocos2d::CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from custom mod!", "OK")->show(); 
-	} 
-};
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * Include the Geode headers.
+ */
+#include <Geode.hpp>
+
+/**
+ * Brings cocos2d and all Geode namespaces 
+ * to the current scope.
+ */
+USE_GEODE_NAMESPACE();
+
+/**
+ * `$modify` lets you extend and modify GD's 
+ * classes; to hook a function in Geode, 
+ * simply $modify the class and write a new 
+ * function definition with the signature of 
+ * the one you want to hook.
+ */
+class $modify(MenuLayer) {
+	/**
+	 * MenuLayer::onMoreGames is a GD function, 
+	 * so we hook it by simply writing its name 
+	 * inside a $modified MenuLayer class.
+	 * 
+	 * Note that for all hooks, your signature 
+	 * has to match exactly - `bool onMoreGames` 
+	 * would not place a hook!
+	 */
+	void onMoreGames(CCObject*) {
+		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show(); 
+	} 
+};
